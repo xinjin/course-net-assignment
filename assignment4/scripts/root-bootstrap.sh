54f48333e2b1ef58d5ab7cf7e7de5c606975ae89
hzhu <hzhu@jhu.edu>
2019-04-13 23:50:47 -0400

    rm comments

--- assignment4/scripts/root-bootstrap.sh
+++ assignment4/scripts/root-bootstrap.sh
@@ -65,28 +65,3 @@ apt-get install -y --no-install-recommends \
   wget \
   xcscope-el \
   xterm
-
-# useradd -m -d /home/p4 -s /bin/bash p4
-# echo "p4:p4" | chpasswd
-# echo "p4 ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/99_p4
-# chmod 440 /etc/sudoers.d/99_p4
-# usermod -aG vboxsf p4
-
-# cd /usr/share/lubuntu/wallpapers/
-# cp /home/vagrant/p4-logo.png .
-# rm lubuntu-default-wallpaper.png
-# ln -s p4-logo.png lubuntu-default-wallpaper.png
-# rm /home/vagrant/p4-logo.png
-# cd /home/vagrant
-# sed -i s@#background=@background=/usr/share/lubuntu/wallpapers/1604-lubuntu-default-wallpaper.png@ /etc/lightdm/lightdm-gtk-greeter.conf
-#
-# # Disable screensaver
-# apt-get -y remove light-locker
-#
-# # Automatically log into the P4 user
-# cat << EOF | tee -a /etc/lightdm/lightdm.conf.d/10-lightdm.conf
-# [SeatDefaults]
-# autologin-user=p4
-# autologin-user-timeout=0
-# user-session=Lubuntu
-# EOF
